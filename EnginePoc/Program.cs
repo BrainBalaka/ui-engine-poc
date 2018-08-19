f7c7ed5a5458bc00b83edab7d59b336ce2045e7e
BrainBalaka <xainsz.balaka@gmail.com>
2018-08-19 11:48:50 +0700

    first commit

---
+++ EnginePoc/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EnginePoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true);
+            var configuration = builder.Build();
+
+            var connection = GetConnection(configuration);
+            Task.Run(() => connection.StartAsync()).Wait();
+
+            int i = 1;
+            while (true)
+            {
+                var message = "test message " + i++;
+                Console.WriteLine(message);
+                Task.Run(() => connection.InvokeAsync("SendMessage", 1, "test", message)).Wait();
+                Thread.Sleep(100);
+            }
+        }
+
+        public static HubConnection GetConnection(IConfigurationRoot config)
+        {
+            var hubUrl = config["HubUrl"];
+            var connection = new HubConnectionBuilder()
+            .WithUrl(hubUrl, options =>
+            {
+                options.AccessTokenProvider = () => Task.FromResult(config["Token"]);
+            })
+            .Build();
+
+            return connection;
+        }
+    }
+}
---
+++ UIPoc/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EnginePoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true);
+            var configuration = builder.Build();
+
+            Console.WriteLine("Enter the job queue id you want to monitor the log:");
+            var jobQueueId = Console.ReadLine();
+            var connection = GetConnection(configuration, jobQueueId);
+
+            // get and display the past log for this job
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse("bearer " + configuration["Token"]);
+            Console.WriteLine(client.GetStringAsync($"{configuration["ApiUrl"]}/queue/{jobQueueId}/logs").Result);
+
+            // listen for the latest log
+            connection.StartAsync().ContinueWith(task => {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}", task.Exception.GetBaseException());
+                }
+                else
+                {
+                    connection.On<string>("ReceiveMessage", (message) =>
+                    {
+                        Console.WriteLine(message);
+                    });
+                }
+
+            }).Wait();
+
+            Console.Read();
+            connection.StopAsync();
+        }
+
+        public static HubConnection GetConnection(IConfigurationRoot config, string jobQueueId)
+        {
+            var hubUrl = config["HubUrl"] + "?jobQueueId=" + jobQueueId;
+            var connection = new HubConnectionBuilder()
+            .WithUrl(hubUrl, options =>
+            {
+                options.AccessTokenProvider = () => Task.FromResult(config["Token"]);
+            })
+            .Build();
+
+            return connection;
+        }
+    }
+}
